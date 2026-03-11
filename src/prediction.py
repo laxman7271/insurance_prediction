f0f3b88f3ef5ad889ad6147647256a3f6d388c75
laxman7271 <luckylaxman7271@gmail.com>
2026-03-11 12:22:26 +0530

    Update prediction.py
    Refactor prediction method to prevent negative values.

--- src/prediction.py
+++ src/prediction.py
@@ -1,17 +1,25 @@
-# 1. load scaler.pkl and model.pkl files
-# 2. create a function to predict
 import pickle
 import numpy as np
+
 class Insurance_Prediction:
+
     def __init__(self):
-        with open("../artifacts/scaler.pki","rb") as f:
+
+        with open("artifacts/scaler.pki", "rb") as f:
             self.scaler = pickle.load(f)
 
-        with open("../artifacts/model.pki","rb") as f:
+        with open("artifacts/model.pki", "rb") as f:
             self.model = pickle.load(f)
 
-    def prediction(self,Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs):
-        input = np.array([[Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs]])
-        scaled_input = self.scaler.transform(input)
-        result = self.model.predict(scaled_input)
-        return result[0]
+    def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
+
+        input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+
+        scaled_input = self.scaler.transform(input_data)
+
+        result = self.model.predict(scaled_input)[0]
+
+        # prevent negative values
+        result = max(result, 0)
+
+        return result
